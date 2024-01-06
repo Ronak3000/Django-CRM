7717e8e6a0f0ca33ee7095254c7d3a0c9e22e7fa
Ronak3000 <ronakeshu3000@gmail.com>
2024-01-06 23:02:35 +0530

    registration

--- website/views.py
+++ website/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
-    #Checking to se Login
+    #Checking to see Login
     if request.method == 'POST':
         username = request.POST[ 'username' ]
         password = request.POST[ 'password' ]
         # authenticate
-        user = authenticate(request,username="username",password="password")
+        user = authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
             messages.success(request,"You Have been Logged in")
@@ -28,4 +29,20 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request,'register.html',{})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,'You are Successfuly Registered')
+            return redirect('home')
+        
+    else:
+        form = SignUpForm()
+        return render(request,'register.html',{'form':form})
+    
+    return render(request, 'register.html', {'form':form})
